bf1ae65618c147dcac30036e3488f873a7bb7707
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-08 21:40:34 -0600

    Add functionality to calculator's add for non-numeric inputs

--- modules/testPractice.js
+++ modules/testPractice.js
@@ -1,12 +1,14 @@
 export function capitalize(string) {
   if (!string) return string;
 
+  //  Replace first char with first char capitalized
   return string.replace(string[0], string[0].toUpperCase());
 }
 
 export function reverseString(string) {
   if (!string) return string;
 
+  //  Split string into array, reverse it and join it back into string
   string = string.split('');
   string.reverse();
   string = string.join('');
@@ -16,6 +18,9 @@ export function reverseString(string) {
 
 export const calculator = (function () {
   function add(x, y) {
+    //  Check if input is a number
+    if (typeof x !== 'number' || typeof y !== 'number') return null;
+
     return x + y;
   }
 
