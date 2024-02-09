fdc8edb93bc5f22cc6db149f4f0730ad8545d9a4
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-08 20:07:12 -0600

    Add test for capitalize with empty string input

---
+++ tests/testPractice.test.js
@@ -0,0 +1,7 @@
+// A capitalize function that takes a string and returns it with the first character capitalized.
+
+import { capitalize } from './testPractice.js';
+
+test('Empty string returns empty string', () => {
+  expect(capitalize('')).toBe('');
+});
