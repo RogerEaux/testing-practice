3b75f12191ed883c6e673eca229e483a2d2d87ff
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-08 20:39:54 -0600

    Move all capitalize test to their own section

--- tests/testPractice.test.js
+++ tests/testPractice.test.js
@@ -2,22 +2,24 @@
 
 import { capitalize } from '../modules/testPractice';
 
-test('Empty string', () => {
-  expect(capitalize('')).toBe('');
-});
+describe('Capitalize', () => {
+  test('Happy path', () => {
+    expect(capitalize('capital')).toBe('Capital');
+  });
 
-test('Happy path', () => {
-  expect(capitalize('capital')).toBe('Capital');
-});
+  test('Empty string', () => {
+    expect(capitalize('')).toBe('');
+  });
 
-test('First element in string is a number', () => {
-  expect(capitalize('42')).toBe('42');
-});
+  test('First element in string is a number', () => {
+    expect(capitalize('42')).toBe('42');
+  });
 
-test('String is null', () => {
-  expect(capitalize(null)).toBe(null);
-});
+  test('String is null', () => {
+    expect(capitalize(null)).toBe(null);
+  });
 
-test('String is one letter', () => {
-  expect(capitalize('f')).toBe('F');
+  test('String is one letter', () => {
+    expect(capitalize('f')).toBe('F');
+  });
 });
