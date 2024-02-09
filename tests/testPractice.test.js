924a33a6209d68fa329bafa2e70611a18a234957
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-09 13:28:30 -0600

    Test one or more elements are not valid nums

--- tests/testPractice.test.js
+++ tests/testPractice.test.js
@@ -181,4 +181,13 @@ describe('Analyze array', () => {
   test('Input is not an array', () => {
     expect(analyzeArray('Array')).toBe(null);
   });
+
+  test('One or more elements are not valid numbers', () => {
+    expect(analyzeArray([1, 8, 3, '4', 2, 6])).toStrictEqual({
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6,
+    });
+  });
 });
